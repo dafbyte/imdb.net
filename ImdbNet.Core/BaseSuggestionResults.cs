58a2db7ec0d9e09c95952ac8b21ad12a3e2b670e
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2018-12-12 21:50:23 +0200

    Refactor suggestion entites (name and titles)

---
+++ ImdbNet.Core/BaseSuggestionResults.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+
+namespace ImdbNet.Core
+{
+	[JsonObject(MemberSerialization.OptIn)]
+	public class BaseSuggestionResults<T>
+		where T : BaseSuggestionResult
+	{
+		[JsonProperty(PropertyName = "v")]
+		public int Version { get; set; }
+
+		[JsonProperty(PropertyName = "q")]
+		public string Query { get; set; }
+
+		[JsonProperty(PropertyName = "d")]
+		public T[] Data { get; set; }
+	}
+
+
+	public class BaseSuggestionResult
+	{
+		[JsonProperty(PropertyName = "id")]
+		public string Id { get; set; }
+
+		[JsonProperty(PropertyName = "l")]
+		public string Title { get; set; }
+	}
+}
--- ImdbNet.Core/NamesSuggestions.cs
+++ ImdbNet.Core/NamesSuggestions.cs
@@ -4,25 +4,16 @@ using Newtonsoft.Json;
 
 namespace ImdbNet.Core
 {
-    /*
+	/*
 {
     "v": 1,
     "q": "jack",
     "d": [ ... ]
     }
      */
-    [JsonObject(MemberSerialization.OptIn)]
-	public class NamesSuggestionsResults
-    {
-        [JsonProperty(PropertyName = "v")]
-        public int Version { get; set; }
+	[JsonObject(MemberSerialization.OptIn)]
+	public class NamesSuggestionsResults : BaseSuggestionResults<NameSuggestion> { }
 
-        [JsonProperty(PropertyName = "q")]
-        public string Query { get; set; }
-
-        [JsonProperty(PropertyName = "d")]
-        public NameSuggestion[] Data { get; set; }
-    }
 
 	/*
         {
@@ -32,20 +23,14 @@ namespace ImdbNet.Core
             "i": [ "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU4MjU0NjI4NF5BMl5BanBnXkFtZTgwNjU3NTUzMDI@._V1_.jpg", 973, 1338 ]
         },
      */
-	public class NameSuggestion
-    {
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
+	public class NameSuggestion : BaseSuggestionResult
+	{
+		[JsonProperty(PropertyName = "s")]
+		public string Summary { get; set; }
 
-        [JsonProperty(PropertyName = "l")]
-        public string Title { get; set; }
+		public string ImagePath => ImageDetails?.ElementAt(0) as string;
 
-        [JsonProperty(PropertyName = "s")]
-        public string Summary { get; set; }
-
-        public string ImagePath => ImageDetails?.ElementAt(0) as string;
-
-        [JsonProperty(PropertyName = "i")]
-        private object[] ImageDetails { get; set; }
-    }
+		[JsonProperty(PropertyName = "i")]
+		private object[] ImageDetails { get; set; }
+	}
 }
