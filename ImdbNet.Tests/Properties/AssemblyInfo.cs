f2c31fd09976b1ac0c19450868a68ce9dd337d04
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2018-12-12 21:51:44 +0200

    Add tests, currently only for name and title suggestions

---
+++ ImdbNet.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImdbNet.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImdbNet.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11cab6fc-11af-4507-b51d-9dc562793806")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ImdbNet.Tests/UtilsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ImdbNet.Core;
+using NUnit.Framework;
+
+
+namespace ImdbNet.Tests
+{
+	[TestFixture]
+	public class UtilsTests
+	{
+		[Test]
+		public static void TestTitlesSuggestions()
+		{
+			var titleSuggestions = Utils.GetTitlesSuggestionsResults(TestQuery);
+			ShowSuggestions(titleSuggestions,
+				s => $"    \"{s.Title}\" ({s.Year}) with {string.Join(", ", s.Stars)} << {s.Id} >>");
+		}
+
+		[Test]
+		public static void TestNamesSuggestions()
+		{
+			var nameSuggestions = Utils.GetNamesSuggestions(TestQuery);
+			ShowSuggestions(nameSuggestions, s => $"    \"{s.Title}\" as {s.Summary} << {s.Id} >>");
+		}
+
+		private static void ShowSuggestions<T>(BaseSuggestionResults<T> suggestions, Func<T, string> itemToString)
+			where T : BaseSuggestionResult
+		{
+			Console.WriteLine(
+				$"Got {suggestions.Data?.Length ?? 0} results for query \"{suggestions.Query}\".");
+			if (suggestions.Data == null)
+				return;
+
+			foreach (var s in suggestions.Data)
+				Console.WriteLine(itemToString(s));
+		}
+
+		private const string TestQuery = "jack";
+	}
+}
