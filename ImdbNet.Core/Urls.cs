ae00abb22ca70a46d8784b7dbed7f4444cd8c5c5
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2018-12-14 22:01:40 +0200

    Extract URLs

---
+++ ImdbNet.Core/Urls.cs
@@ -0,0 +1,8 @@
+﻿namespace ImdbNet.Core
+{
+	public static class Urls
+	{
+		public const string TitleSuggestions = "https://v2.sg.media-imdb.com/suggests/titles";
+		public const string NameSuggestions = "https://v2.sg.media-imdb.com/suggests/names";
+	}
+}
